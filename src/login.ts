e504ad31f7aa451eef1073b69479e208826a01bb
tochibedford <musicofbyte@gmail.com>
2023-08-10 12:49:47 +0100

    feat: switch to gmail login

--- src/login.ts
+++ src/login.ts
@@ -1,16 +1,35 @@
 import nodemailer from "nodemailer";
 
-async function emailLoginTransporter() {
-  const testAccount = await nodemailer.createTestAccount();
-  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth: {
-      user: testAccount.user,
-      pass: testAccount.pass,
-    },
-  });
+export async function emailLoginTransporter(config?: {
+  email?: string;
+  pass?: string;
+}) {
+  let transporter: ReturnType<typeof nodemailer.createTransport>;
+  switch (config) {
+    case undefined:
+      {
+        transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false,
+          auth: {
+            // user: testAccount.user,
+            user: "hailee.medhurst@ethereal.email",
+            pass: "dRFqPn4jUETVcAEryN",
+          },
+        });
+      }
+      break;
+    default:
+      transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: config.email,
+          pass: config.pass,
+        },
+      });
+      break;
+  }
 
   return transporter;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,28 @@
-async function main() {}
+const clipboard = import("clipboardy");
+import "dotenv/config";
+import { emailLoginTransporter } from "./login";
+
+const appPassword = process.env["GMAIL_PASS"];
+const gmailEmail = process.env["GMAIL_EMAIL"];
+
+let lastClipBoardValue = "";
+
+async function copyFromClipboard() {}
+
+async function main() {
+  const clipText = (await clipboard).default.readSync();
+  const transporter = await emailLoginTransporter({
+    email: gmailEmail,
+    pass: appPassword,
+  });
+  const info = await transporter.sendMail({
+    from: "Tochi Foo 😵 <tochieatsbeats@gmail.com>",
+    to: "tochieatsbeats@gmail.com",
+    subject: "copyPaste : Initial Email",
+    text: clipText,
+  });
+
+  console.log(info);
+}
 
 main();
