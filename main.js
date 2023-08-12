b4d37d7ea989b71329ca8d404330a4d7f7cf2f75
tochibedford <musicofbyte@gmail.com>
2023-08-12 18:58:30 +0100

    feat bluetooth setup with web API

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+let bluetoothPinCallback;
+let selectBluetoothCallback;
 
 function createMainWindow() {
   const win = new BrowserWindow({
     width: 500,
     height: 300,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.webContents.on(
+    "select-bluetooth-device",
+    (event, deviceList, callback) => {
+      event.preventDefault();
+      selectBluetoothCallback = callback;
+      const result = deviceList[0];
+
+      if (result) {
+        callback(result.deviceId);
+      } else {
+      }
+    }
+  );
+
+  ipcMain.on("cancel-bluetooth-request", (event) => {
+    selectBluetoothCallback("");
+  });
+
+  ipcMain.on("bluetooth-pairing-response", (_, response) => {
+    bluetoothPinCallback(response);
+  });
+
+  win.webContents.session.setBluetoothPairingHandler((details, callback) => {
+    bluetoothPinCallback = callback;
+    win.webContents.send("bluetooth-pairing-request", details);
   });
 
   win.loadFile("ui/index.html");
--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,50 @@
-console.log("heyyy");
+const locateDevicesButton = document.getElementById("locate");
+
+locateDevicesButton.addEventListener("click", () => {
+  navigator.bluetooth
+    .requestDevice({ acceptAllDevices: true })
+    .then((device) => {
+      console.log(device);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+});
+
+function cancelRequest() {
+  window.electronAPI.cancelBluetoothRequest();
+}
+
+document.getElementById("cancel").addEventListener("click", cancelRequest);
+
+window.electronAPI.bluetoothPairingRequest((event, details) => {
+  const response = {};
+
+  switch (details.pairingKind) {
+    case "confirm": {
+      response.confirmed = window.confirm(
+        `Do you want to connect to device ${details.deviceId}?`
+      );
+      break;
+    }
+    case "confirmPin": {
+      response.confirmed = window.confirm(
+        `Does the pin ${details.pin} match the pin displayed on device ${details.deviceId}?`
+      );
+      break;
+    }
+    case "providePin": {
+      const pin = window.prompt(
+        `Please provide a pin for ${details.deviceId}.`
+      );
+      if (pin) {
+        response.pin = pin;
+        response.confirmed = true;
+      } else {
+        response.confirmed = false;
+      }
+    }
+  }
+
+  window.electronAPI.bluetoothPairingResponse(response);
+});
